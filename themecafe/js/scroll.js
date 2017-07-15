4346a6472d472046bab8e6869dbfdd99df05e2ba
Andrey <zookwer@yandex.ru>
2017-07-15 13:33:42 +0300

    Add files via upload

---
+++ themecafe/js/scroll.js
@@ -0,0 +1,6 @@
+﻿!function($){
+              $(document).on('click', 'a[href^=#]', function () {
+              $('html, body').animate({ scrollTop:  $('a[name="'+this.hash.slice(1)+'"]').offset().top }, 1200 ); 
+              return false;
+              });
+              }(jQuery);
